edefbc468f3e56025cebce46c5a836a6f618fc3f
Your Name <you@example.com>
2024-08-16 17:17:08 +0530

    Your commit message here

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Adding the WebSocket time display -->
+    <div>
+      <h2>{{ time }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      time: null
+    };
+  },
+  mounted() {
+    let connection = new WebSocket('ws://localhost:3000/');
+    connection.onmessage = (event) => {
+      this.time = event.data;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
